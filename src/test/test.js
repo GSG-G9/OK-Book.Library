2156a77eb63437958a5998d54c7512068abb924b
khamis-hesham <khamis1997.kh@gmail.com>
2020-12-02 14:37:28 +0200

    add description

--- src/test/test.js
+++ src/test/test.js
@@ -1,3 +1,7 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable no-undef */
+/* eslint-disable consistent-return */
 const request = require('supertest');
 const app = require('../app');
 
