9fda03c89c4166f371da6ad47533cb9e6ac03b95
khamis-hesham <khamis1997.kh@gmail.com>
2020-12-03 14:42:33 +0200

    fix bugs

--- src/controllers/api/bookSearch.js
+++ src/controllers/api/bookSearch.js
@@ -1,7 +1,12 @@
 const { bookApi } = require('../../utils/apiData/bookFetch');
 
 const bookSearch = (req, res) => {
-  const { searchInput } = req.params;
+  const searchInput = req.query.q;
+  if (!searchInput) {
+    return res.status(401).send({
+      msg: 'q is required',
+    });
+  }
   bookApi.search(searchInput)
     .then((data) => res.status(200).send(data))
     .catch((err) => res.status(500).send(err));
